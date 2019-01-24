1176d585976cdeca017980543c8a8429cc56bc49
zhengyanweb <173760795@qq.com>
2019-01-24 23:04:27 +0800

    zy 2019-1-24

--- gulpfile.config.js
+++ gulpfile.config.js
@@ -1,30 +1,34 @@
-var SRC_DIR = './src/';   //源文件目录
-var DIST_DIR = './dist/';  //文件处理后存放的目录
+var SRC_DIR = './src/'; //源文件目录
+var DIST_DIR = './dist/'; //文件处理后存放的目录
 var DIST_FILES = DIST_DIR + '**'; // 目标路径下的所有文件
 
 var Config = {
-	src: SRC_DIR,
-	dist: DIST_DIR,
-	dist_files: DIST_FILES,
-	html: {
-		src: SRC_DIR + 'page/*.html',
-		dist: DIST_DIR + 'page/'
-	},
-	less: {
-		src: SRC_DIR + 'assets/less/*.less',
-		dist: DIST_DIR + 'assets/css'
-	},
-	fonts: {
-		src: SRC_DIR + 'assets/fonts/*.*',
-		dist: DIST_DIR + 'assets/fonts'
-	},
-	js: {
-		src: SRC_DIR + 'assets/js/**/*',             // JS目录：./src/js/  
-		dist: DIST_DIR + 'assets/js',
-	},
-	img: {
-		src: SRC_DIR + 'assets/images/**/*',
-		dist: DIST_DIR + 'assets/images'
-	}
+    src: SRC_DIR,
+    dist: DIST_DIR,
+    dist_files: DIST_FILES,
+    html: {
+        src: SRC_DIR + 'page/*.html',
+        dist: DIST_DIR + 'page/'
+    },
+    less: {
+        src: SRC_DIR + 'assets/less/*.less',
+        dist: DIST_DIR + 'assets/css'
+    },
+    fonts: {
+        src: SRC_DIR + 'assets/fonts/*.*',
+        dist: DIST_DIR + 'assets/fonts'
+    },
+    js: {
+        src: SRC_DIR + 'assets/js/**/*', // JS目录：./src/js/  
+        dist: DIST_DIR + 'assets/js',
+    },
+    img: {
+        src: SRC_DIR + 'assets/images/**/*',
+        dist: DIST_DIR + 'assets/images'
+    },
+    other: {
+        src: SRC_DIR + 'favicon.ico',
+        dist: DIST_DIR
+    }
 }
 module.exports = Config;
--- gulpfile.js
+++ gulpfile.js
@@ -10,61 +10,78 @@ var imagemin = require('gulp-imagemin'); //图片压缩
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var Config = require('./gulpfile.config.js');
+var clean = require('gulp-clean');
+var pump = require('pump');
 
 
 /* dev html处理 */
-gulp.task('html:dev', function () {
-	gulp.src(Config.html.src)
-		.pipe(gulp.dest(Config.html.dist))
-		.pipe(reload({ stream: true }))
+gulp.task('html:dev', function() {
+    gulp.src(Config.html.src)
+        .pipe(gulp.dest(Config.html.dist))
+        .pipe(reload({ stream: true }))
 });
 
 /* dev less处理 */
-gulp.task('less:dev', function () {
-	gulp.src(Config.less.src)
-		.pipe(less())
-		.pipe(gulp.dest(Config.less.dist))
-		.pipe(reload({ stream: true }))
+gulp.task('less:dev', function() {
+    gulp.src(Config.less.src)
+        .pipe(less())
+        .pipe(gulp.dest(Config.less.dist))
+        .pipe(reload({ stream: true }))
 });
 
 /* dev 字体处理 */
-gulp.task('fonts:dev', function () {
-	gulp.src(Config.fonts.src)
-		.pipe(gulp.dest(Config.fonts.dist))
-		.pipe(reload({ stream: true }))
+gulp.task('fonts:dev', function() {
+    gulp.src(Config.fonts.src)
+        .pipe(gulp.dest(Config.fonts.dist))
+        .pipe(reload({ stream: true }))
 });
 
 /* 图片处理 */
-gulp.task('images:dev', function () {
-	gulp.src(Config.img.src)
-		.pipe(imagemin({
-			optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
-			progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
-			interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-			multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
-		}))
-		.pipe(gulp.dest(Config.img.dist))
-		.pipe(reload({ stream: true }));
+gulp.task('images:dev', function() {
+    gulp.src(Config.img.src)
+        .pipe(imagemin({
+            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+        }))
+        .pipe(gulp.dest(Config.img.dist))
+        .pipe(reload({ stream: true }));
 })
+
+/* favicon.ico */
+gulp.task('other:dev', function() {
+    gulp.src(Config.other.src)
+        .pipe(gulp.dest(Config.other.dist))
+})
+
 
 /* js处理 */
-gulp.task('js:dev', function () {
-	gulp.src(Config.js.src)
-		.pipe(gulp.dest(Config.js.dist))
-		.pipe(reload({ stream: true }));
+gulp.task('js:dev', function() {
+    gulp.src(Config.js.src)
+        .pipe(gulp.dest(Config.js.dist))
+        .pipe(reload({ stream: true }));
 })
 
-gulp.task('dev', ['html:dev', 'less:dev', 'images:dev', 'fonts:dev', 'js:dev'], function () {
-	browserSync.init({
-		port: new Date().getFullYear(),
-		server: {
-			baseDir: './dist/',
-			index: "page/index.html"
-		},
-		notify: false
-	});
-	gulp.watch(Config.html.src, ['html:dev']);
-	gulp.watch(Config.less.src, ['less:dev']);
-	gulp.watch(Config.img.src, ['images:dev']);
-	gulp.watch(Config.js.src, ['js:dev']);
+/* 删除 */
+gulp.task('del', function(cb) {
+    pump([
+        gulp.src('./dist'),
+        clean()
+    ], cb)
+})
+
+gulp.task('dev', ['html:dev', 'less:dev', 'images:dev', 'fonts:dev', 'js:dev', 'other:dev'], function() {
+    browserSync.init({
+        port: new Date().getFullYear(),
+        server: {
+            baseDir: './dist/',
+            index: "page/index.html"
+        },
+        notify: false
+    });
+    gulp.watch(Config.html.src, ['html:dev']);
+    gulp.watch(Config.less.src, ['less:dev']);
+    gulp.watch(Config.img.src, ['images:dev']);
+    gulp.watch(Config.js.src, ['js:dev']);
 })
